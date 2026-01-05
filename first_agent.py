09c210d33e4daad4f5036ce7e5e6b5b17743ed96
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-01-05 19:22:12 +0530

    create tool and bind with model

---
+++ first_agent.py
@@ -0,0 +1,40 @@
+from langchain.tools import tool
+from langchain.chat_models import init_chat_model
+
+chat_model = init_chat_model(model_name="gpt-4o", temperature=0)
+
+# Define tools
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a * b
+
+
+@tool
+def add(a: int, b: int) -> int:
+    """Adds `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a + b
+
+
+@tool
+def divide(a: int, b: int) -> float:
+    """Divide `a` and `b`.
+
+    Args:
+        a: First int
+        b: Second int
+    """
+    return a / b
+
+tools = [multiply, add, divide]
+agent = chat_model.bind(tools)
