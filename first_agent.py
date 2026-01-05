74ca7ce4a41c8bcf9414e1b773f71060673b5c36
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-01-05 19:38:12 +0530

    add nodes and condition edges

--- first_agent.py
+++ first_agent.py
@@ -1,5 +1,10 @@
 from langchain.tools import tool
 from langchain.chat_models import init_chat_model
+from langchain.messages import AnyMessage, SystemMessage, ToolMessage
+from type_extensions import TypeDict, Annotated
+from typing import Literal
+from langgraph.graph import StateGraph, START, END
+import operator
 
 chat_model = init_chat_model(model_name="gpt-4o", temperature=0)
 
@@ -38,3 +43,50 @@ def divide(a: int, b: int) -> float:
 
 tools = [multiply, add, divide]
 agent = chat_model.bind(tools)
+
+class MassagesState(TypeDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    llm_calls: int
+
+
+
+def llm_call(state: dict):
+    """LLM decides whether to call a tool or not"""
+
+    return {
+        "messages": [
+            model_with_tools.invoke(
+                [
+                    SystemMessage(
+                        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                    )
+                ]
+                + state["messages"]
+            )
+        ],
+        "llm_calls": state.get('llm_calls', 0) + 1
+    }
+
+def tool_node(state: dict):
+    """Performs the tool call"""
+
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call["args"])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+
+
+def should_continue(state: MessagesState) -> Literal["tool_node", END]:
+    """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+
+    messages = state["messages"]
+    last_message = messages[-1]
+
+    # If the LLM makes a tool call, then perform an action
+    if last_message.tool_calls:
+        return "tool_node"
+
+    # Otherwise, we stop (reply to the user)
+    return END
